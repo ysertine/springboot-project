8b7c9937add9a3368bf2fe5aad65a90e5d55689d
dengjinbo <335196272@qq.com>
2018-12-25 11:13:20 +0800

    整合Thymeleaf模板；

---
+++ src/main/java/com/ysertine/system/controller/ThymeleafTestController.java
@@ -0,0 +1,52 @@
+package com.ysertine.system.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ysertine.system.entity.ThymeleafTest;
+
+/**
+ * @Title ThymeleafController.java
+ * @Description Thymeleaf模板测试控制类
+ * @author DengJinbo
+ * @date 2018年12月25日
+ */
+@Controller
+@RequestMapping
+public class ThymeleafTestController {
+	
+	@GetMapping("/index")
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        // 设置跳转的视图 默认映射到 src/main/resources/templates/{viewName}.html
+        view.setViewName("thymeleaf");
+        // 设置属性
+        view.addObject("title", "SpringBoot Project页面");
+        view.addObject("desc", "欢迎进入 SpringBoot Project 系统");
+        ThymeleafTest author = new ThymeleafTest();
+        author.setAge(18);
+        author.setEmail("123456@qq.com");
+        author.setName("大波波");
+        view.addObject("author", author);
+        return view;
+    }
+
+    @GetMapping("/index1")
+    public String index1(HttpServletRequest request) {
+        // 与上面的写法不同，但是结果一致。
+        // 设置属性
+        request.setAttribute("title", "SpringBoot Project页面");
+        request.setAttribute("desc", "欢迎进入 SpringBoot Project 系统");
+        ThymeleafTest author = new ThymeleafTest();
+        author.setAge(18);
+        author.setEmail("123456@qq.com");
+        author.setName("大波波");
+        request.setAttribute("author", author);
+        // 返回的 index 默认映射到 src/main/resources/templates/xxxx.html
+        return "thymeleaf";
+    }
+}
