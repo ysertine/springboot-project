12f8aafe8f6960ae153a305c1d2c31ebf40b74b8
dengjinbo <335196272@qq.com>
2019-01-15 16:54:17 +0800

    抽取通用Service接口以及其实现方法；

---
+++ src/main/java/com/ysertine/common/service/BaseService.java
@@ -0,0 +1,132 @@
+package com.ysertine.common.service;
+
+import java.util.List;
+
+/**
+ * @Title BaseService.java
+ * @Description 通用的Service接口
+ * @author DengJinbo
+ * @date 2019年1月15日
+ */
+public interface BaseService<T> {
+
+	/**
+	 * @Title save 
+	 * @Description 保存一个实体，null的属性也会保存，不会使用数据库默认值
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int save(T record);
+	
+	/**
+	 * @Title saveSelective 
+	 * @Description 保存一个实体，null的属性也不会保存，会使用数据库默认值
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int saveSelective(T record);
+	
+	/**
+	 * @Title delete 
+	 * @Description 根据实体属性作为条件进行删除，查询条件使用等号
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int delete(T record);
+	
+	/**
+	 * @Title deleteByPrimaryKey 
+	 * @Description 根据主键字段进行删除，方法参数必须包含完整的主键属性
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param key
+	 * @return
+	 */
+	int deleteByPrimaryKey(Object key);
+	
+	/**
+	 * @Title updateByPrimaryKey 
+	 * @Description 根据主键更新实体全部字段，null值会被更新
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int updateByPrimaryKey(T record);
+	
+	/**
+	 * @Title updateByPrimaryKeySelective 
+	 * @Description 根据主键更新属性不为null的值
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int updateByPrimaryKeySelective(T record);
+	
+	/**
+	 * @Title getByPrimaryKey 
+	 * @Description 根据主键字段进行查询，方法参数必须包含完整的主键属性，查询条件使用等号
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param key
+	 * @return
+	 */
+	T getByPrimaryKey(Object key);
+	
+	/**
+	 * @Title getByCriteria 
+	 * @Description 根据实体中的属性进行查询，只能有一个返回值，有多个结果是抛出异常，查询条件使用等号
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	T getByCriteria(T record);
+	
+	/**
+	 * @Title listByCriteria 
+	 * @Description 根据实体中的属性值进行查询，查询条件使用等号
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	List<T> listByCriteria(T record);
+	
+	/**
+	 * @Title listAll 
+	 * @Description 查询全部结果，select(null)方法能达到同样的效果
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @return
+	 */
+	List<T> listAll();
+	
+	/**
+	 * @Title countByCriteria 
+	 * @Description 根据实体中的属性查询总数，查询条件使用等号
+	 * @author DengJinbo
+	 * @date 2019年1月15日
+	 * @version 1.0
+	 * @param record
+	 * @return
+	 */
+	int countByCriteria(T record);
+}
---
+++ src/main/java/com/ysertine/common/service/impl/BaseServiceImpl.java
@@ -0,0 +1,76 @@
+package com.ysertine.common.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ysertine.common.service.BaseService;
+
+import tk.mybatis.mapper.common.BaseMapper;
+
+/**
+ * @Title BaseServiceImpl.java
+ * @Description 通用的Service接口实现类
+ * @author DengJinbo
+ * @date 2019年1月15日
+ */
+public abstract class BaseServiceImpl<T> implements BaseService<T> {
+
+	@Autowired
+	private BaseMapper<T> baseMapper;
+	
+	@Override
+	public int save(T record) {
+		return baseMapper.insert(record);
+	}
+
+	@Override
+	public int saveSelective(T record) {
+		return baseMapper.insertSelective(record);
+	}
+
+	@Override
+	public int delete(T record) {
+		return baseMapper.delete(record);
+	}
+
+	@Override
+	public int deleteByPrimaryKey(Object key) {
+		return baseMapper.deleteByPrimaryKey(key);
+	}
+
+	@Override
+	public int updateByPrimaryKey(T record) {
+		return baseMapper.updateByPrimaryKey(record);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(T record) {
+		return baseMapper.updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public T getByPrimaryKey(Object key) {
+		return baseMapper.selectByPrimaryKey(key);
+	}
+
+	@Override
+	public T getByCriteria(T record) {
+		return baseMapper.selectOne(record);
+	}
+
+	@Override
+	public List<T> listByCriteria(T record) {
+		return baseMapper.select(record);
+	}
+
+	@Override
+	public List<T> listAll() {
+		return baseMapper.selectAll();
+	}
+
+	@Override
+	public int countByCriteria(T record) {
+		return baseMapper.selectCount(record);
+	}
+}
