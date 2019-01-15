a2d7b2b8419049f0e9599c1012a285e21302e4c9
dengjinbo <335196272@qq.com>
2019-01-15 17:12:40 +0800

    整合后台页面框架，调整后台首页；

--- src/main/resources/static/common/commonCss.js
+++
@@ -1,3 +0,0 @@
-﻿document.write('<link rel="stylesheet" href="/dist/css/adminlte.min.css">');
-document.write('<link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css">');
-document.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">');
--- src/main/resources/static/common/commonJs.js
+++
@@ -1,3 +0,0 @@
-﻿document.write('<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>');
-document.write('<script type="text/javascript" src="/dist/js/adminlte.min.js"></script>');
